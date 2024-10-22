a35f02e6e2a75f0e9d961246ea5879054f549310
Kevin Mueller <kevin.mueller@tuta.com>
2024-10-22 07:59:27 +0200

    added newsletter_abo to models.py and migrated

--- sales/models.py
+++ sales/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Customer(models.Model):
-    first_name= models.CharField(max_length=30)
-    last_name= models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    newsletter_abo = models.BooleanField(default=False)
+    # email_address = models.EmailField()
+    # account = models.FloatField()
