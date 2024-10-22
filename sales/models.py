30e4b49d337c6a0e9df5aaff4ffc1a6efcfedbc3
Kevin Mueller <kevin.mueller@tuta.com>
2024-10-22 08:06:50 +0200

    added two more fields to model

--- sales/models.py
+++ sales/models.py
@@ -7,5 +7,5 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     newsletter_abo = models.BooleanField(default=False)
-    # email_address = models.EmailField()
-    # account = models.FloatField()
+    email_address = models.CharField(max_length=30,blank=True,default="")
+    account = models.FloatField(blank=True, null=True)
