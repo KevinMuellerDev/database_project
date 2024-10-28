7f4ba03011f58fd3ac9a324eadf9d95ae5b0c49a
Kevin Mueller <kevin.mueller@tuta.com>
2024-10-28 06:32:00 +0100

    added customeradmin config

--- sales/admin.py
+++ sales/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
 from .models import *
 
-model=[Product,Bill,Order,Producttype]
+model = [Product, Bill, Order, Producttype]
 
 # Register your models here.
-class AdminCustomer(admin.ModelAdmin):
-    list_filter=["first_name", "last_name"]
-    
 
-admin.site.register(Customer,AdminCustomer)
+
+class AdminCustomer(admin.ModelAdmin):
+    list_filter = ["first_name", "last_name"]
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": ["first_name","last_name","account"],
+            },
+        ),
+        (
+            "Advanced options",
+            {
+                "classes": ["collapse"],
+                "fields": ["newsletter_abo"],
+            },
+        ),
+    ]
+
+
+admin.site.register(Customer, AdminCustomer)
 admin.site.register(model)
