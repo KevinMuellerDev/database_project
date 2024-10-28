47b896fa9cf9b7a0fd8ca8f22b0ed641057b6c75
Kevin Mueller <kevin.mueller@tuta.com>
2024-10-28 06:15:02 +0100

    added list_filter

--- sales/admin.py
+++ sales/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import *
 
-model=[Customer,Product,Bill,Order,Producttype]
+model=[Product,Bill,Order,Producttype]
 
 # Register your models here.
+class AdminCustomer(admin.ModelAdmin):
+    list_filter=["first_name", "last_name"]
+    
+
+admin.site.register(Customer,AdminCustomer)
 admin.site.register(model)
