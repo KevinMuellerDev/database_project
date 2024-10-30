76e9f4d69fdd908f79cec5adbba0bf54a53fb506
Kevin Mueller <kevin.mueller@tuta.com>
2024-10-30 14:05:44 +0100

    added comments to code

--- sales/views.py
+++ sales/views.py
@@ -8,9 +8,9 @@ class CustomerListView(ListView):
     template_name = "sales/list.html"
     
 class CustomerListSearchView(CustomerListView):
-    #aufbereitung der daten um nur die passende bedingung zu rendern
+    #Aufbereitung der daten um nur die passende bedingung zu rendern
     def get_queryset(self):
-       #name ist der mitgegebene parameter in der url 
+        #name ist der mitgegebene parameter in der url 
         name=self.kwargs.get("name")
         # returned nur die values auf die die bedingung zutrifft und agiert von da an weiter wie die customer list view da diese extended wird
         return Customer.objects.filter(first_name__icontains=name)  
